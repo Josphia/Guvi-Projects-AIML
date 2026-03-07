df4e677e103f2a9e7d25aafe4232a12750ba5672
Josphia <josphiagladlin@gmail.com>
2026-03-08 01:14:22 +0530

    EDA

--- Book Recommendation System/Main_Page.py
+++ Book Recommendation System/Main_Page.py
@@ -2,8 +2,11 @@ import os
 import pandas as pd
 import numpy as np
 import streamlit as st
+import plotly.express as px
 
 st.set_page_config(layout='wide')
+
+
 
 base_path = os.path.dirname(__file__)
 
@@ -30,13 +33,36 @@ df = df.dropna(subset=['Rating_x'])
 df['Popularity'] = df['Rating_x'] * df['Number of Reviews']
 df.columns = ['Book Name', 'Author Name', 'Rating', 'Price', 'Description', 'Listening Time', 'Ranks and Genre', 'Number of Reviews', 'Popularity']
 
-st.sidebar.subheader("EDA")
+with st.sidebar:
+    page = st.radio("Go to", ['Home', 'EDA', 'NLP'])
+
+if page == "Home":
+    st.title("You are in Home Page")
+
+elif page == "EDA":
+    st.subheader("Exploratory Data Analysis (EDA)")
+    optionselected = st.selectbox("Select one from Below", ['None', 'Ratings Distribution', 
+                                             'Top 10 Popular Books',
+                                             'Top 5 Authors'])
+    if optionselected == "Ratings Distribution":
+        min_Rating = df['Rating'].min()
+        max_Rating = df['Rating'].max()
+        rating_Selected = st.slider("Select a Rating",min_value=min_Rating, max_value=max_Rating, value=max_Rating, step=0.1)
+        result_df = df[df['Rating']==rating_Selected]
+        st.dataframe(result_df, hide_index=True)
+    
+    elif optionselected == "Top 10 Popular Books":
+        result_df = df.sort_values(by='Popularity', ascending=False)
+        st.dataframe(result_df[['Book Name','Author Name','Description', 'Number of Reviews']].head(10),hide_index=True)
+
+    elif optionselected == "Top 5 Authors":
+        result_df = df['Author Name'].value_counts().head(5).reset_index()
+        result_df.columns = ["Author Name", "No. of Books"]
+        fig = px.pie(data_frame=result_df,names='Author Name',values='No. of Books',title='Top 5 Authors')
+        st.plotly_chart(fig)
+
+elif page == "NLP":
+    st.error("No")
 
 
-st.subheader("Hii")
-name = st.text_input("Enter your Name")
-submit = st.button("Submit")
-if submit and name:
-    st.success(f"Hii {name}..")
-
-#st.dataframe(df, hide_index=True)
+st.dataframe(df, hide_index=True)
