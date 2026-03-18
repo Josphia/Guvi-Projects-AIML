38743dfdd66a656d02681f5dc69ecde11b386a60
Josphia <josphiagladlin@gmail.com>
2026-03-18 10:29:09 +0530

    After avatar

--- Customer_Support_GenAI/Main_Page.py
+++ Customer_Support_GenAI/Main_Page.py
@@ -69,7 +69,12 @@ if "messages" not in st.session_state:
     st.session_state.messages = []
 
 for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
+    if message["role"] == "user":
+        current_avatar = "user.png"
+    else:
+        current_avatar = "bot.png"
+    
+    with st.chat_message(message["role"], avatar=current_avatar):
         st.markdown(message["content"])
 
 if prompt := st.chat_input("Type your issue here..."):
